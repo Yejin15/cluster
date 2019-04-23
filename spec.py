f23c5f6f9cc39d62f980927ff3794f019ec50fb8
Your Name Here <dpwls273@naver.com>
2019-04-23 15:33:58 +0900

    agglomerative, specular incurs error

---
+++ spec.py
@@ -0,0 +1,23 @@
+import json
+import sklearn.cluster as clu
+import numpy as np
+import pandas as pd
+
+cluster = 4
+
+with open('/Users/maeg/PycharmProjects/fisrtApp/Total_labels/long_sample.json') as json_file:
+    json_data = json.load(json_file)
+    X = pd.DataFrame.from_records(json_data)
+    X = pd.DataFrame.transpose(X)
+    X = pd.DataFrame.drop(X, ['name'])
+    X = pd.DataFrame.fillna(X, value=0)
+    X_out = pd.DataFrame.to_string(X)
+
+    model = clu.spectral_clustering(X, n_clusters=cluster)
+
+    result=pd.DataFrame(X[0])
+
+    result[0] = model[1]
+    print(result)
+    result.to_csv('/Users/maeg/PycharmProjects/fisrtApp/result/spec_sample.csv')
+
